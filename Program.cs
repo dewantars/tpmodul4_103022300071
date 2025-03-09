3f943233e6072f6edde267f3115eb783b3898405
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-03-09 12:33:07 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using tpmodul4_103022300071;
+
+namespace tpmodul4_103022300071
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            KodePos kodePos = new KodePos();
+
+            kodePos.DisplayAllKodePos();
+
+        }
+    }
+}
