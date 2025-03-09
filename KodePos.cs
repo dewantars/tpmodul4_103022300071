d6a3e495a577df0cc362de7bf506273e525cb18f
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-03-09 12:05:39 +0700

    inisialisasi projek

---
+++ KodePos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022300071
+{
+    class KodePos
+    {
+    }
+}
